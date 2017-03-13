ec63d0dbe37aec4a64b4040980b3f6a576dadaa9
Alan Nosenkis <nalanpa@yandex.ru>
2017-03-13 19:07:39 +0200

    2

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//import readlineSync from 'readline-sync';
+
+//const actual = readlineSync.question('Your answer: ');
+
+
+export const askName = () => {
+  console.log('In askName');
+}
+
+/*
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+*/
