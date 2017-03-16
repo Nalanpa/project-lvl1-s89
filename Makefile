bba41f655a00d0ce6685c169e2853b6032e65b01
Alan Nosenkis <nalanpa@yandex.ru>
2017-03-16 11:03:49 +0200

    9 checkpoint

--- Makefile
+++ Makefile
@@ -19,6 +19,9 @@ balance:
 progression:
 	npm run babel-node -- 'src/bin/brain-progression.js'
 
+prime:
+	npm run babel-node -- 'src/bin/brain-prime.js'
+
 publish:
 	npm publish
 
@@ -29,4 +32,4 @@ build:
 	rm -rf dist
 	npm run build
 
-.PHONY: install start publish lint build even calc gcd balance progression
+.PHONY: install start publish lint build even calc gcd balance progression prime
---
+++ src/games/prime.js
@@ -0,0 +1,30 @@
+import { cons } from 'hexlet-pairs';
+import gameStart from '../game';
+
+
+const isPrime = (n) => {
+  if (n === 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const task = () => {
+  const maxNumber = 1000;
+  const num = Math.floor(Math.random() * maxNumber);
+
+  const isNumPrime = (isPrime(num)) ? 'yes' : 'no';
+
+  return cons(num, isNumPrime);
+};
+
+export default () => {
+  const greetingString = 'Answer "yes" if number prime otherwise answer "no".';
+
+  gameStart(greetingString, task);
+};
